0055065e545707095aa8de35caa95ad7e467f41d
Andrew Jarombek <ajarombek95@gmail.com>
2018-09-12 14:25:29 -0400

    Logo Assets

--- s3-assets/main.tf
+++ s3-assets/main.tf
@@ -449,3 +449,304 @@ resource "aws_s3_bucket_object" "12-30-17-xmlresponsetext-png" {
 /*
  * Logos Directory
  */
+
+resource "aws_s3_bucket_object" "angular-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/angular.png"
+  source = "assets/logos/angular.png"
+  etag = "${md5(file("assets/logos/angular.png"))}"
+}
+
+resource "aws_s3_bucket_object" "apigateway-svg" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/apigateway.svg"
+  source = "assets/logos/apigateway.svg"
+  etag = "${md5(file("assets/logos/apigateway.svg"))}"
+}
+
+resource "aws_s3_bucket_object" "aws-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/aws.png"
+  source = "assets/logos/aws.png"
+  etag = "${md5(file("assets/logos/aws.png"))}"
+}
+
+resource "aws_s3_bucket_object" "awslambda-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/awslambda.png"
+  source = "assets/logos/awslambda.png"
+  etag = "${md5(file("assets/logos/awslambda.png"))}"
+}
+
+resource "aws_s3_bucket_object" "babel-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/babel.png"
+  source = "assets/logos/babel.png"
+  etag = "${md5(file("assets/logos/babel.png"))}"
+}
+
+resource "aws_s3_bucket_object" "batch-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/batch.png"
+  source = "assets/logos/batch.png"
+  etag = "${md5(file("assets/logos/batch.png"))}"
+}
+
+resource "aws_s3_bucket_object" "bootstrap-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/bootstrap.png"
+  source = "assets/logos/bootstrap.png"
+  etag = "${md5(file("assets/logos/bootstrap.png"))}"
+}
+
+resource "aws_s3_bucket_object" "c-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/c.png"
+  source = "assets/logos/c.png"
+  etag = "${md5(file("assets/logos/c.png"))}"
+}
+
+resource "aws_s3_bucket_object" "css-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/css.png"
+  source = "assets/logos/css.png"
+  etag = "${md5(file("assets/logos/css.png"))}"
+}
+
+resource "aws_s3_bucket_object" "ec2-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/ec2.png"
+  source = "assets/logos/ec2.png"
+  etag = "${md5(file("assets/logos/ec2.png"))}"
+}
+
+resource "aws_s3_bucket_object" "es6-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/es6.png"
+  source = "assets/logos/es6.png"
+  etag = "${md5(file("assets/logos/es6.png"))}"
+}
+
+resource "aws_s3_bucket_object" "es2017-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/es2017.png"
+  source = "assets/logos/es2017.png"
+  etag = "${md5(file("assets/logos/es2017.png"))}"
+}
+
+resource "aws_s3_bucket_object" "express-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/express.png"
+  source = "assets/logos/express.png"
+  etag = "${md5(file("assets/logos/express.png"))}"
+}
+
+resource "aws_s3_bucket_object" "github-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/github.png"
+  source = "assets/logos/github.png"
+  etag = "${md5(file("assets/logos/github.png"))}"
+}
+
+resource "aws_s3_bucket_object" "graphql-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/graphql.png"
+  source = "assets/logos/graphql.png"
+  etag = "${md5(file("assets/logos/graphql.png"))}"
+}
+
+resource "aws_s3_bucket_object" "groovy-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/groovy.png"
+  source = "assets/logos/groovy.png"
+  etag = "${md5(file("assets/logos/groovy.png"))}"
+}
+
+resource "aws_s3_bucket_object" "gulp-svg" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/gulp.svg"
+  source = "assets/logos/gulp.svg"
+  etag = "${md5(file("assets/logos/gulp.svg"))}"
+}
+
+resource "aws_s3_bucket_object" "haskell-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/haskell.png"
+  source = "assets/logos/haskell.png"
+  etag = "${md5(file("assets/logos/haskell.png"))}"
+}
+
+resource "aws_s3_bucket_object" "html-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/html.png"
+  source = "assets/logos/html.png"
+  etag = "${md5(file("assets/logos/html.png"))}"
+}
+
+resource "aws_s3_bucket_object" "java-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/java.png"
+  source = "assets/logos/java.png"
+  etag = "${md5(file("assets/logos/java.png"))}"
+}
+
+resource "aws_s3_bucket_object" "java8-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/java8.png"
+  source = "assets/logos/java8.png"
+  etag = "${md5(file("assets/logos/java8.png"))}"
+}
+
+resource "aws_s3_bucket_object" "js-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/js.png"
+  source = "assets/logos/js.png"
+  etag = "${md5(file("assets/logos/js.png"))}"
+}
+
+resource "aws_s3_bucket_object" "json-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/json.png"
+  source = "assets/logos/json.png"
+  etag = "${md5(file("assets/logos/json.png"))}"
+}
+
+resource "aws_s3_bucket_object" "jwt-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/jwt.png"
+  source = "assets/logos/jwt.png"
+  etag = "${md5(file("assets/logos/jwt.png"))}"
+}
+
+resource "aws_s3_bucket_object" "mongodb-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/mongodb.png"
+  source = "assets/logos/mongodb.png"
+  etag = "${md5(file("assets/logos/mongodb.png"))}"
+}
+
+resource "aws_s3_bucket_object" "mongoose-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/mongoose.png"
+  source = "assets/logos/mongoose.png"
+  etag = "${md5(file("assets/logos/mongoose.png"))}"
+}
+
+resource "aws_s3_bucket_object" "neo4j-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/neo4j.png"
+  source = "assets/logos/neo4j.png"
+  etag = "${md5(file("assets/logos/neo4j.png"))}"
+}
+
+resource "aws_s3_bucket_object" "nodejs-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/nodejs.png"
+  source = "assets/logos/nodejs.png"
+  etag = "${md5(file("assets/logos/nodejs.png"))}"
+}
+
+resource "aws_s3_bucket_object" "oracle-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/oracle.png"
+  source = "assets/logos/oracle.png"
+  etag = "${md5(file("assets/logos/oracle.png"))}"
+}
+
+resource "aws_s3_bucket_object" "php-svg" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/php.svg"
+  source = "assets/logos/php.svg"
+  etag = "${md5(file("assets/logos/php.svg"))}"
+}
+
+resource "aws_s3_bucket_object" "python-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/python.png"
+  source = "assets/logos/python.png"
+  etag = "${md5(file("assets/logos/python.png"))}"
+}
+
+resource "aws_s3_bucket_object" "rabbitmq-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/rabbitmq.png"
+  source = "assets/logos/rabbitmq.png"
+  etag = "${md5(file("assets/logos/rabbitmq.png"))}"
+}
+
+resource "aws_s3_bucket_object" "react-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/react.png"
+  source = "assets/logos/react.png"
+  etag = "${md5(file("assets/logos/react.png"))}"
+}
+
+resource "aws_s3_bucket_object" "sass-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/sass.png"
+  source = "assets/logos/sass.png"
+  etag = "${md5(file("assets/logos/sass.png"))}"
+}
+
+resource "aws_s3_bucket_object" "sql-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/sql.png"
+  source = "assets/logos/sql.png"
+  etag = "${md5(file("assets/logos/sql.png"))}"
+}
+
+resource "aws_s3_bucket_object" "swift-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/swift.png"
+  source = "assets/logos/swift.png"
+  etag = "${md5(file("assets/logos/swift.png"))}"
+}
+
+resource "aws_s3_bucket_object" "tech_logos-svg" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/tech_logos.svg"
+  source = "assets/logos/tech_logos.svg"
+  etag = "${md5(file("assets/logos/tech_logos.svg"))}"
+}
+
+resource "aws_s3_bucket_object" "terraform-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/terraform.png"
+  source = "assets/logos/terraform.png"
+  etag = "${md5(file("assets/logos/terraform.png"))}"
+}
+
+resource "aws_s3_bucket_object" "travisci-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/travisci.png"
+  source = "assets/logos/travisci.png"
+  etag = "${md5(file("assets/logos/travisci.png"))}"
+}
+
+resource "aws_s3_bucket_object" "ts-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/ts.png"
+  source = "assets/logos/ts.png"
+  etag = "${md5(file("assets/logos/ts.png"))}"
+}
+
+resource "aws_s3_bucket_object" "vim-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/vim.png"
+  source = "assets/logos/vim.png"
+  etag = "${md5(file("assets/logos/vim.png"))}"
+}
+
+resource "aws_s3_bucket_object" "webpack-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/webpack.png"
+  source = "assets/logos/webpack.png"
+  etag = "${md5(file("assets/logos/webpack.png"))}"
+}
+
+resource "aws_s3_bucket_object" "yaml-png" {
+  bucket = "${aws_s3_bucket.asset-jarombek.id}"
+  key = "logos/yaml.png"
+  source = "assets/logos/yaml.png"
+  etag = "${md5(file("assets/logos/yaml.png"))}"
+}
